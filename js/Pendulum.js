745fac95376bce17025f073836449b184cf9732e
Sikriti Dakua <destrodev123@gmail.com>
2020-06-02 00:16:07 +0530

    updated

--- js/Pendulum.js
+++ js/Pendulum.js
@@ -8,6 +8,7 @@ class Pendulum {
 		this.position = options.position;
 		this.startAngle = options.startAngle;
 		this.length = options.length;
+		this.sphereMaterialProps = options.sphereMaterialProps;
 
 		this.angle = Math.PI;
 		this.angleVelocity = 0;
@@ -36,7 +37,12 @@ class Pendulum {
 		this.hook = new THREE.Mesh(hookGeometry, metalMaterial);
 
 		// The wire that holds the sphere
-		const wireGeometry = new THREE.CylinderGeometry(0.03, 0.03, this.length, 12);
+		const wireGeometry = new THREE.CylinderGeometry(
+			0.03,
+			0.03,
+			this.length,
+			12
+		);
 		wireGeometry.translate(0, -this.length / 2, 0);
 		this.wire = new THREE.Mesh(wireGeometry, metalMaterial);
 
@@ -46,32 +52,53 @@ class Pendulum {
 
 		// The sphere
 		const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-		const sphereMaterial = new THREE.MeshLambertMaterial({
-			color: 0xffdf40, // ffac40,
-			emissive: 0xbd1816, // ff1414,
-			emissiveIntensity: 10,
-		});
-		this.sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+		this.sphereMaterial = new THREE.MeshStandardMaterial(
+			this.sphereMaterialProps
+		);
+		this.sphere = new THREE.Mesh(sphereGeometry, this.sphereMaterial);
+	}
 
-		// Add the meshes to othe Scene
+	addToScene() {
+		// Add the meshes to the Scene
 		this.scene.add(this.hook);
 		this.scene.add(this.wire);
 		this.scene.add(this.torus);
 		this.scene.add(this.sphere);
 	}
 
+	removeFromScene() {
+		this.scene.remove(this.wire);
+		this.scene.remove(this.torus);
+		this.scene.remove(this.sphere);
+	}
+
 	updateAngles() {
-		this.angleAcceleration = 4 * (0.001 + this.phaseDiff) * Math.sin(this.angle);
+		this.angleAcceleration =
+			2 * (0.001 + this.phaseDiff) * Math.sin(this.angle);
 		this.angleVelocity += this.angleAcceleration;
 		this.angle += this.angleVelocity;
 	}
 
 	updatePosition() {
-		this.current.x = this.position.x + this.origin.x + this.length * Math.sin(this.angle);
-		this.current.y = this.position.y + this.origin.y + this.length * Math.cos(this.angle);
+		this.current.x =
+			this.position.x +
+			this.origin.x +
+			this.length * Math.sin(this.angle);
+		this.current.y =
+			this.position.y +
+			this.origin.y +
+			this.length * Math.cos(this.angle);
 
-		this.sphere.position.set(this.current.x, this.current.y, this.position.z + this.z);
-		this.torus.position.set(this.current.x, this.current.y, this.position.z + this.z);
+		this.sphere.position.set(
+			this.current.x,
+			this.current.y,
+			this.position.z + this.z
+		);
+		this.torus.position.set(
+			this.current.x,
+			this.current.y,
+			this.position.z + this.z
+		);
 		this.wire.position.set(
 			this.position.x,
 			this.position.y + this.length,
@@ -85,6 +112,14 @@ class Pendulum {
 
 		this.wire.lookAt(this.sphere.position);
 		this.wire.rotateX(-Math.PI / 2);
+	}
+
+	updateSphereMaterial(props) {
+		this.sphereMaterial.color = new THREE.Color(props.color);
+		this.sphereMaterial.emissive = new THREE.Color(props.emissive);
+		this.sphereMaterial.emissiveIntensity = props.emissiveIntensity;
+		this.sphereMaterial.metalness = props.metalness;
+		this.sphereMaterial.roughness = props.roughness;
 	}
 }
 
